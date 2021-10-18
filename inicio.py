0dd9b06b622578d2028148852ec0c4cdd54f0985
Djaysel Pessôa <djaysel@outlook.com>
2021-10-18 07:55:54 -0300

    Add files via upload

---
+++ inicio.py
@@ -0,0 +1,12 @@
+from defCore.Escolha import ObjEscolha
+from defCore.Mensagem import ObjMess
+
+
+class Main:
+    def __init__(self):
+        ObjMess.baner(0)
+
+        ObjEscolha.inicializar()
+
+
+ObjMain = Main()
